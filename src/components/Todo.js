0f68fef87a7da693679599994408a8e04054942d
Hugh Caluscusin <hugh@melodiccrypter.com>
2019-06-07 19:51:23 +0800

    modified Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 
 class Todo extends Component {
   render() {
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#cdcdcd',
+      textDecoration: 'line-through',
+    };
+
     return (
-      <div>
-          <input/>
+      // The DIV for the todo item container
+      <div
+          style={{display: !this.props.item.todo && 'none'}}
+          className="todo-item"
+      >
+
+          {/* the checkbox beside the todo item */}
+          <input
+              type="checkbox"
+              checked={this.props.item.completed}
+              onChange={() => this.props.handleChange(this.props.item.id)}
+          />
+
+          {/* the todo item itself */}
+          <p style={this.props.item.completed ? completedStyle : null}>
+              {this.props.item.todo}
+          </p>
+
       </div>
     )
   }
