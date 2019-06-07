1801f4cd43460cd9c72ade93279d4957ad0988d3
Hugh Caluscusin <hugh@melodiccrypter.com>
2019-06-07 19:50:58 +0800

    modified Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,13 +8,38 @@ class Main extends Component {
         super();
         this.state = {
             todos: todos,
-        }
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(id) {
+        this.setState((prevState) => {
+            const updatedTodo = prevState.todos.map((todo) => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+
+                return todo;
+            });
+
+            return {
+                todos: updatedTodo,
+            }
+        });
     }
 
     render() {
+        const todoComponent = this.state.todos.map((todo) => {
+            return <Todo
+                key={todo.id}
+                item={todo}
+                handleChange={this.handleChange}
+            />
+        });
+
         return (
           <main>
-
+              {todoComponent}
           </main>
         )
     }
