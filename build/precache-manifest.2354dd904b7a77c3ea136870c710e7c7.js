863c1fd7a846852e9e7cfa37d6772364c9763519
Hugh Caluscusin <hugh@melodiccrypter.com>
2019-07-08 15:37:50 +0800

    public deployment, added build dir

---
+++ build/precache-manifest.2354dd904b7a77c3ea136870c710e7c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e34bd0092e2ff285c954aa868415ff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa7d2b0d8adfab3ccefc",
+    "url": "/static/css/main.52192dd8.chunk.css"
+  },
+  {
+    "revision": "ad30c0be719298edcd8b",
+    "url": "/static/js/2.a022f2a8.chunk.js"
+  },
+  {
+    "revision": "aa7d2b0d8adfab3ccefc",
+    "url": "/static/js/main.9ed4dae2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
